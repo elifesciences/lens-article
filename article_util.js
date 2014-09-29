2d7478ef961a53c340faa31392bc8aa89c7ba2f9
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-09-29 16:33:09 +0200

    Fix date parsing: use explicit constructor instead of dash format.
    E.g., new Date("2004-07-11") does not work under Safari and FF.

--- article_util.js
+++ article_util.js
@@ -18,13 +18,15 @@ var util = {};
 util.formatDate = function (pubDate) {
   var parts = pubDate.split("-");
   if (pubDate.split("-").length >= 3) {
-    var localDate = new Date(pubDate);
-    return localDate.toUTCString().slice(0, 16)
+    // new Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
+    // Note: months are 0-based
+    var localDate = new Date(parts[0], parts[1]-1, parts[2]);
+    return localDate.toUTCString().slice(0, 16);
   } else {
     var month = parts[1].replace("0", "");
     var year = parts[0];
     return MONTH_MAPPING[month]+" "+year;
   }
-}
+};
 
 module.exports = util;
