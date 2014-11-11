cfe9e6ec4ac2afdb4452edd035cfabd2ff672579
Michael Aufreiter <michael@substance.io>
2014-11-11 19:17:36 +0100

    Consider section labels.

--- nodes/heading/heading_view.js
+++ nodes/heading/heading_view.js
@@ -1,19 +1,40 @@
 "use strict";
 
 var TextView = require('../text').View;
+var NodeView = require("../node").View;
+var $$ = require("substance-application").$$;
+
 
 // Substance.Heading.View
 // ==========================================================================
 
-var HeadingView = function() {
-  TextView.apply(this, arguments);
+var HeadingView = function(node, viewFactory) {
+  NodeView.call(this, node, viewFactory);
 
   this.$el.addClass('level-'+this.node.level);
 };
 
-HeadingView.Prototype = function() {};
+HeadingView.Prototype = function() {
+  this.render = function() {
+    NodeView.prototype.render.call(this);
 
-HeadingView.Prototype.prototype = TextView.prototype;
+    // Heading title
+    var titleView = this.createTextView({
+      path: [this.node.id, 'content'],
+      classes: 'title'
+    });
+
+    if (this.node.label) {
+      var labelEl = $$('.label', {text: this.node.label});
+      this.content.appendChild(labelEl);
+    }
+
+    this.content.appendChild(titleView.render().el);
+    return this;
+  };
+};
+
+HeadingView.Prototype.prototype = NodeView.prototype;
 HeadingView.prototype = new HeadingView.Prototype();
 
 module.exports = HeadingView;
