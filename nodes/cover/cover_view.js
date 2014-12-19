157c9b8ea60c2b4263a980a9ac40ed2343acd9f7
Michael Aufreiter <michael@substance.io>
2014-12-19 13:22:15 +0100

    DOI is now stored without url prefix.

--- nodes/cover/cover_view.js
+++ nodes/cover/cover_view.js
@@ -122,7 +122,7 @@ CoverView.Prototype = function() {
       var doi = pubInfo.doi;
       if (doi) {
         this.content.appendChild($$('.doi', {
-          html: 'DOI: <a href="'+doi+'">'+doi+'</a>'
+          html: 'DOI: <a href="http://dx.doi.org/'+doi+'">'+doi+'</a>'
         }));
       }
     }
